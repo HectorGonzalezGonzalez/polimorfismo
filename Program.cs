8ab29582abb9778999da4984cd2df5fc638e1d68
HectorGonzalezGonzalez <37567557+HectorGonzalezGonzalez@users.noreply.github.com>
2020-07-29 12:32:47 -0500

    cambio de prueba dos

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace polimorfismo
             Decimal fBase = 5;
             Decimal altura = 7;
             Decimal areaRectangulo=new rectangulo().area(fBase,altura);//usa el polimosfismo
-            Decimal areaTriangulo = new Triangulo().area(fBase, altura);//no aplica el polimosfismo pero utiliza e metodo heredado
+            Decimal areaTriangulo = new Triangulo().area(fBase, altura);//no aplica el polimosfismo pero utiliza el metodo heredado
             Console.WriteLine($"Area del rectangulo:{areaRectangulo}");
             Console.WriteLine($"Area del triangulo:{areaTriangulo}");
             Console.ReadKey();
